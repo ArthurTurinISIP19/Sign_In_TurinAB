10ce13a83c81bca92733cce19c4dfba480d71c9d
Turin Arthur <ezeka1n@mail.ru>
2021-02-07 12:22:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sign_in_turinab/Second.java
@@ -0,0 +1,16 @@
+package com.example.sign_in_turinab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.sign_in_turinab.R;
+
+
+public class Second extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
